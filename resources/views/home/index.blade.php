69691411e41b0f8b7b96b03b608e03e83b66203c
alicevik06 <62955191+alicevik06@users.noreply.github.com>
2021-09-23 16:42:21 +0300

    Framework Şablon

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,13 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>First Laravel Page</title>
-</head>
-<body>
+@extends('layouts.home')
 
-<h1>Welcome To Laravel</h1>
-<p>This is a paragraph.</p>
-<a href="{{route('test' ,['id' => 1])}}">Test</a>
+@section('title', 'Sub Page Title')
 
-</body>
-</html>
+@section('sidebar')
+
+    <div style="background-color:olive;height: 160px;" class="col-md-4">
+        <ul>
+            <li>Bilgisayar</li>
+            <li>Elektronik</li>
+            <li>Ev eşyası</li>
+
+        </ul>
+    </div>
+
+@endsection
+
+@section('slider')
+    <div style="background-color:lightgreen;height: 160px;" class="col-md-8">
+        <h1>Slider</h1>
+        asdasdasdasd
+    </div>
+@endsection
+
+
+@section('content')
+    <div style="background-color: lightsalmon;height: 150px;text-align: center">
+        <h1>İçerik Alanı</h1>
+        ürünler
+        <br>
+        ürün A
+    </div>
+@endsection
---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>@yield('title')</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    @yield('css')
+    @yield('js')
+</head>
+<body>
+<!-- Üstmenü Alanı alanı  -->
+
+<div style="background-color:coral;margin-top: 0px;height: 150px;" class="jumbotron text-center">
+    <h2>Header</h2>
+    <p>Sayfa Üst Kısım</p>
+    @yield('header')
+</div>
+
+<!-- Menü ve Sidebar alanı  -->
+
+<div class="container">
+    <div class="row">
+        @yield('sidebar')
+        @yield('slider')
+    </div>
+</div>
+
+<!-- İçerik alanı  -->
+<div class="container">
+    <div class="row text-center">
+        @yield('content')
+    </div>
+</div>
+
+<!-- AltMenü alanı  -->
+
+<div style="height: 150px;background-color: darkkhaki" class="jumbotron text-center">
+    <h2>Footer</h2>
+    <p>Sayfa Alt Kısım</p>
+    @yield('footer')
+</div>
+
+</body>
+</html>
